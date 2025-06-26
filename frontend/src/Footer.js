e2bdb85ad9c6f4eb63f9b22faacefc0891f9cffe
Myself-Alien <pritimaypanda1993@gmail.com>
2025-06-26 09:10:19 +0530

    Footer Style Change

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,6 +2,7 @@ const Footer = () =>
 {
     return(
         <div className="container-fluid p-3 mt-4 footer">
+        <hr />
         <span><b>© 2025 Movie Search App | Created by Pritimay</b></span>
         </div>
     );
